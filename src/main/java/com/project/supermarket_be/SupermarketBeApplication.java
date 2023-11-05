0201dea1d0b4d0e26bc1356b98ee6748ee41d6c3
duclongdev <20521566@gm.uit.edu.vn>
2023-11-05 16:46:22 +0700

    create project structure

--- src/main/java/com/project/supermarket_be/SupermarketBeApplication.java
+++ src/main/java/com/project/supermarket_be/SupermarketBeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SupermarketBeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SupermarketBeApplication.class, args);
 	}
-
 }
